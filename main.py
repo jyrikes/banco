e36c394df7901a7adbe2ac14952b2089d0796fc5
netuh <waldemar.neto@gmail.com>
2022-12-11 11:56:18 -0300

    aula2

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+
+soma = 0  # acumulador das idade
+quantidade = 0  # acumulador da quantidade
+idade = int(input("digite a idade:"))
+while (idade >= 0):
+    soma = soma + idade
+    quantidade = quantidade + 1
+    idade = int(input("digite a idade:"))
+else:
+    print("fim")
+print("a soma das idade é", soma)
+print("a quantidade de valores digitados foi", quantidade)
+media = soma/quantidade
+print("a media é", media)
